c19d008e8bfa849f6b74fef6ee021a03f0a17f9a
hgfs113 <dr.dmitrii2000@yandex.ru>
2020-12-27 02:21:12 +0300

    Добавлена возможность создания и обучения модели

--- Task 3/ml_server.py
+++ Task 3/ml_server.py
@@ -15,7 +15,14 @@ app = Flask(__name__, template_folder='html')
 
 datasets = {}
 data_info = {}
+models = {}
+params = {}
 message = ""
+message_use = ""
+train_RMSE = 0
+train_time = 0
+test_RMSE = 0
+loss_fname = ""
 
 
 @app.route('/')
@@ -28,6 +35,14 @@ def index():
 def get_index():
     return '<html><center><script>document.write("hello from flask server")</script></center> </html>'
 
+
+@app.route('/clear_graphics', methods=['POST'])
+def clear_graphics():
+    global train_RMSE
+    train_RMSE = 0
+    if os.path.isfile('static/' + loss_fname):
+            os.remove('static/' + loss_fname)
+    return redirect(url_for('index'))
 
 
 @app.route('/datasets', methods=['GET', 'POST'])
@@ -76,3 +91,84 @@ def prepare_data():
 
         return redirect(url_for('prepare_data'))
     return render_template('datasets.html', message=message, data_info=data_info)
+
+
+@app.route('/models', methods=['GET', 'POST'])
+def prepare_model():
+    global train_RMSE, loss_fname, train_time
+    if request.method == 'POST':
+        # parse params
+        try:
+            n_estimators = int(request.form['n_estimators'])
+        except:
+            n_estimators = 100
+
+        try:
+            max_depth = int(request.form['max_depth'])
+        except:
+            if request.form['select_model'] == 'RandomForest':
+                max_depth = None
+            else:
+                max_depth = 5
+
+        try:
+            feature_subsample_size = int(request.form['feature_subsample_size'])
+        except:
+            feature_subsample_size = None
+
+        try:
+            learning_rate = float(request.form['learning_rate'])
+        except:
+            learning_rate = 0.1
+
+        params[request.form['model_name']] = {"n_estimators":n_estimators, 
+                "max_depth":max_depth, "feature_subsample_size":feature_subsample_size,
+                "dataset":request.form['select_dataset']}
+
+        # create model
+        if request.form['select_model'] == 'RandomForest':
+            models[request.form['model_name']] = ensembles.RandomForestMSE(
+                    n_estimators, max_depth=max_depth, feature_subsample_size=feature_subsample_size
+                )
+
+        else:
+            models[request.form['model_name']] = ensembles.GradientBoostingMSE(
+                    n_estimators, max_depth=max_depth, feature_subsample_size=feature_subsample_size,
+                    learning_rate=learning_rate
+                )
+            params[request.form['model_name']]["learning_rate"] = learning_rate
+
+        # fit model
+        st = time()
+        X, y = datasets[request.form['select_dataset']]
+        models[request.form['model_name']].fit(X.values, y.values)
+        train_time = time() - st
+        # grafics
+        model = models[request.form['model_name']]
+        loss = []
+        if os.path.isfile('static/' + loss_fname):
+            os.remove('static/' + loss_fname)
+        fig_loss = plt.figure(figsize=(6, 4))
+        if request.form['select_model'] == 'RandomForest':
+            plt.title("RandomForest, RMSE")
+            rez = np.zeros(X.values.shape[0])
+            for i in range(model.n_estimators):
+                rez += model.forest[i].predict(X.values[:, model.feat_subsamps[i]])
+                loss.append(mean_squared_error(rez / (i + 1), y.values, squared=False))
+        else:
+            plt.title("GradientBoosting, RMSE")
+            rez = model.f_0*np.ones(X.values.shape[0])
+            for i in range(model.M):
+                a, coef = model.estimators[i][0]
+                rez -= coef*model.l_r*a.predict(X.values[:, model.feat_subsamps[i]])
+                loss.append(mean_squared_error(rez, y.values, squared=False))
+        plt.plot(loss)
+        plt.xlabel("n_estimators")
+        plt.ylabel("RMSE")
+        loss_plot = True
+        loss_fname = "loss_" + request.form['model_name'] + str(np.random.randint(1, 1000)) + ".jpg"
+        fig_loss.savefig('static/' + loss_fname)
+        train_RMSE = mean_squared_error(models[request.form['model_name']].predict(X.values), y.values, squared=False)
+        return redirect(url_for('prepare_model'))
+    return render_template('models.html', datasets=datasets, test=round(train_RMSE, 2),
+            time=round(train_time, 2), loss_fname=loss_fname)
